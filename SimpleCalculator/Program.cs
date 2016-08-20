c48e6ab48bc285a526a954ec504fc6ec210bc510
Sylvia Ma <xiangwma@umich.edu>
2016-08-20 10:55:42 -0500

    constant first part, repeatly input, counter

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -11,53 +11,98 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.Write("[X]>");
-            string inputString = Console.ReadLine();
-            string pattern = @"(?<digit1>[0-9]+)(?<operation>(\+|\-|\*|\/))(?<digit2>[0-9]+)";
-            Match m = Regex.Match(inputString, pattern);
-            if(m.Success==true)
+            int counter = 0;
+            while (true)
             {
-                int number1 = int.Parse(m.Groups["digit1"].Value);
-                int number2 = int.Parse(m.Groups["digit2"].Value);
-                string operation = m.Groups["operation"].Value;
-                if(operation=="+")
-                {
-                    Addition addition = new Addition();
-                    int additionResult = addition.AdditionOperation(number1, number2);
-                    Console.WriteLine("="+additionResult);
 
-                    Modulus modulus = new Modulus();
-                    int modulusResult = modulus.ModulusFunction(-8);
-                    Console.WriteLine(modulusResult);
-                }else if(operation=="-")
+                Console.Write("[" + counter + "]>");
+
+                string inputString = Console.ReadLine();
+                string pattern1 = @"(?<digit1>[0-9]+)(?<operation>(\+|\-|\*|\/))(?<digit2>[0-9]+)";
+                string pattern2 = @"\d";
+                string pattern3 = @"(?<constantLetter>[a-zA-Z])(\s?)\=(\s?)(?<constantNumber>\d+)";
+                string exitPattern = @"(exit|quit)";
+
+                Match exit = Regex.Match(inputString.ToLower(), exitPattern);
+                if (exit.Success == true)
                 {
-                    Subtraction subtraction = new Subtraction();
-                    int subtractionResult = subtraction.SubtractionOperation(number1, number2);
-                    Console.WriteLine("=" + subtractionResult);
-                }else if(operation=="*")
-                {
-                    Multiplication multi = new Multiplication();
-                    int multiResult = multi.MultiplicationOperation(number1, number2);
-                    Console.WriteLine("=" + multiResult);
-                }else if(operation=="/")
-                {
-                    Division divi = new Division();
-                    double diviResult = divi.DivisionOperation(number1, number2);
-                    Console.WriteLine("=" + diviResult);
-                        
-                       
-         
+                    break;
                 }
-      
+
+                Match m1 = Regex.Match(inputString, pattern1);
+                if (m1.Success == true)
+                {
+                    int number1 = int.Parse(m1.Groups["digit1"].Value);
+                    int number2 = int.Parse(m1.Groups["digit2"].Value);
+                    string operation = m1.Groups["operation"].Value;
+                    if (operation == "+")
+                    {
+                        Addition addition = new Addition();
+                        int additionResult = addition.AdditionOperation(number1, number2);
+                        Console.WriteLine("=" + additionResult);
+                        counter++;
+                        continue;
+
+                        //Modulus modulus = new Modulus();
+                        //int modulusResult = modulus.ModulusFunction(-8);
+                        //Console.WriteLine(modulusResult);
+                    }
+                    else if (operation == "-")
+                    {
+                        Subtraction subtraction = new Subtraction();
+                        int subtractionResult = subtraction.SubtractionOperation(number1, number2);
+                        Console.WriteLine("=" + subtractionResult);
+                        counter++;
+                        continue;
+                    }
+                    else if (operation == "*")
+                    {
+                        Multiplication multi = new Multiplication();
+                        int multiResult = multi.MultiplicationOperation(number1, number2);
+                        Console.WriteLine("=" + multiResult);
+                        counter++;
+                        continue;
+                    }
+                    else if (operation == "/")
+                    {
+                        Division divi = new Division();
+                        double diviResult = divi.DivisionOperation(number1, number2);
+                        Console.WriteLine("=" + diviResult);
+                        counter++;
+                        continue;
+                    }
+
+                }
+
+                Match constantOperation = Regex.Match(inputString, pattern3);
+                if (constantOperation.Success == true)
+                {
+                    Console.WriteLine("Constant");
+                    string consLetter = constantOperation.Groups["constantLetter"].Value;
+                    double consNumber = int.Parse(constantOperation.Groups["constantNumber"].Value);
+                    Console.WriteLine("Saved '" + consLetter + "' as '" + consNumber + "'");
+                    counter++;
+                    continue;
+
+
+                }
+                //1. repeat
+                //2. counter
+                //3. catch error
+                //4. constant
+                //5. modulus
+                //6. last
+
+
+
+
+                //string digitString2=Regex.Match(inputString, );
+
+                //Console.WriteLine(digitString1);
             }
-
-
-            //string digitString2=Regex.Match(inputString, );
-
-            //Console.WriteLine(digitString1);
-            Console.WriteLine("End");
-            Console.ReadLine();
-
+                Console.WriteLine("End");
+                Console.ReadLine();
+            
         }
     }
 }
